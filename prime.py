daf2025e191ce7d58ab48329e6d7f79cea0ba9d3
Dhiraj Kumar <123482915+Erdhiraj123@users.noreply.github.com>
2023-08-02 11:34:11 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,3 +1,5 @@
+#To Prime in Interval
+
 for x in range(2,20):
     count=0
     for i in range(2,x//2+1):
@@ -6,3 +8,15 @@ for x in range(2,20):
             break
     if count==0:
         print(x)
+        
+# To Check wheathe a number prime or not
+
+prime=int(input("Enter a number:"))
+count=0
+for x in range(2,prime//2+1):
+    if prime%x==0:
+        count=count+1
+if count==0:
+    print("Prime number")
+else:
+    print("Not prime ")
